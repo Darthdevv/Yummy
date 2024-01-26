4a3d5ae693f79290ea3d7f3755925ed028ea7c7a
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-01-26 19:14:16 +0200

    🥘 updated

--- js/index.js
+++ js/index.js
@@ -1,3 +1,46 @@
 ///<reference types="../@types/jquery" />
 "use strict";
 
+
+
+
+let categories = [];
+
+
+getCategories();
+
+async function getCategories() {
+  const response = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/categories.php`
+  );
+  const meals = await response.json();
+  categories = meals.categories;
+  console.log(categories);
+  displayData();
+}
+
+
+
+function displayData() {
+  let cols = '';
+  categories.map(
+    (item) =>
+      (cols += `
+      <div class="col-md-3">
+                  <div class="meal position-relative overflow-hidden rounded-2 cursor-pointer">
+                      <img class="w-100" src="${
+                        item.strCategoryThumb
+                      }" alt="" srcset="">
+                      <div class="meal-layer position-absolute text-center text-black p-2">
+                          <h3>${item.strCategory}</h3>
+                          <p>${item.strCategoryDescription
+                            .split(" ")
+                            .slice(0, 20)
+                            .join(" ")}</p>
+                      </div>
+                  </div>
+          </div>
+  `)
+  );
+  document.getElementById('rowData').innerHTML = cols;
+}
