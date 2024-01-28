7a39a939d131bedbe8d9c93f9955259277a6403e
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-01-28 02:02:04 +0200

    🥘 updated

--- js/index.js
+++ js/index.js
@@ -64,14 +64,13 @@ function closeSideNav() {
 }
 closeSideNav();
 
-$(".side-nav-menu i.open-close-icon").click(() => {
-  if ($(".side-nav-menu").css("left") == "0px") {
+$(document).on("click", ".side-nav-menu i.open-close-icon", function () {
+  if ($(".side-nav-menu").css("left") === "0px") {
     closeSideNav();
   } else {
     openSideNav();
   }
 });
-
 
 async function getCategories() {
   rowData.innerHTML = "";
@@ -297,5 +296,133 @@ function displayContactsInputs() {
   `;
   searchContainer.innerHTML = '';
   rowData.innerHTML = '';
+
+    submitBtn = document.getElementById("submitBtn")
+
+
+    document.getElementById("nameInput").addEventListener("focus", () => {
+        nameInputTouched = true
+    })
+
+    document.getElementById("emailInput").addEventListener("focus", () => {
+        emailInputTouched = true
+    })
+
+    document.getElementById("phoneInput").addEventListener("focus", () => {
+        phoneInputTouched = true
+    })
+
+    document.getElementById("ageInput").addEventListener("focus", () => {
+        ageInputTouched = true
+    })
+
+    document.getElementById("passwordInput").addEventListener("focus", () => {
+        passwordInputTouched = true
+    })
+
+    document.getElementById("repasswordInput").addEventListener("focus", () => {
+        repasswordInputTouched = true
+    })
 }
 
+let nameInputTouched = false;
+let emailInputTouched = false;
+let phoneInputTouched = false;
+let ageInputTouched = false;
+let passwordInputTouched = false;
+let repasswordInputTouched = false;
+
+
+
+
+function inputsValidation() {
+    if (nameInputTouched) {
+        if (nameValidation()) {
+            document.getElementById("nameAlert").classList.replace("d-block", "d-none")
+
+        } else {
+            document.getElementById("nameAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+    if (emailInputTouched) {
+
+        if (emailValidation()) {
+            document.getElementById("emailAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("emailAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+
+    if (phoneInputTouched) {
+        if (phoneValidation()) {
+            document.getElementById("phoneAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("phoneAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+
+    if (ageInputTouched) {
+        if (ageValidation()) {
+            document.getElementById("ageAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("ageAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+
+    if (passwordInputTouched) {
+        if (passwordValidation()) {
+            document.getElementById("passwordAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("passwordAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+    if (repasswordInputTouched) {
+        if (repasswordValidation()) {
+            document.getElementById("repasswordAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("repasswordAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+
+
+    if (nameValidation() &&
+        emailValidation() &&
+        phoneValidation() &&
+        ageValidation() &&
+        passwordValidation() &&
+        repasswordValidation()) {
+        submitBtn.removeAttribute("disabled")
+    } else {
+        submitBtn.setAttribute("disabled", true)
+    }
+}
+
+function nameValidation() {
+    return (/^[a-zA-Z ]+$/.test(document.getElementById("nameInput").value))
+}
+
+function emailValidation() {
+    return (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(document.getElementById("emailInput").value))
+}
+
+function phoneValidation() {
+    return (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(document.getElementById("phoneInput").value))
+}
+
+function ageValidation() {
+    return (/^(0?[1-9]|[1-9][0-9]|[1][1-9][1-9]|200)$/.test(document.getElementById("ageInput").value))
+}
+
+function passwordValidation() {
+    return (/^(?=.*\d)(?=.*[a-z])[0-9a-zA-Z]{8,}$/.test(document.getElementById("passwordInput").value))
+}
+
+function repasswordValidation() {
+    return document.getElementById("repasswordInput").value == document.getElementById("passwordInput").value
+}
